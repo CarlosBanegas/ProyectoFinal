c8a1d14fb107c19f29155ac7f7c5ee1525930e49
ARVARELA <allan.varela@ujcv.edu.hn>
2016-04-15 02:02:12 -0600

    Formulario de nuevo vehiculo

---
+++ ACESubastas/app/src/main/java/com/example/arvarela/acesubastas/RgisterCarActivity.java
@@ -0,0 +1,13 @@
+package com.example.arvarela.acesubastas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RgisterCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rgister_car);
+    }
+}
